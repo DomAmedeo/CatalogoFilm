43a3ba21887699dcc8f95ad3a495a45b2da5fe89
domenico <dsablone@itsmeccanicabruzzo.eu>
2025-09-12 18:17:27 +0200

    Creazione classe Film e Main

---
+++ Film.java
@@ -0,0 +1,16 @@
+public class Film{
+    private int id;
+    private String titolo;
+    private String regista;
+    private int anno;
+    private String genere;
+
+    public Film (int id, String titolo, String regista, int anno, String genere){
+        this.id = id;
+        this.titolo = titolo;
+        this.regista = regista;
+        this.anno= anno;
+        this.genere= genere;
+    }
+    
+}
